d7ed6f7ae5221026ab4a51f9283971710c7a063f
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-11-07 14:50:00 -0600

    Re-factored code, replaced push level system in favor of smaller iterator,  css still looks bad

--- js/main.js
+++ js/main.js
@@ -1,81 +1,51 @@
-//---------------variables(states)-------------------------
-
-//---------------ARRAYS-------------------------
-const levelArray = []; 
-
-// const userArray = []; //for user answers
-
-//--------------------EVENTS-----------------------
-// var nameHere = document.getElementById('id');
-
-//START BTN 
-var startBtn = document.getElementById('start');
-
-//--------------------LISTENERS--------------------------
-// object.addEventListener(event, function)
-startBtn.addEventListener('click', levelZero);
-startBtn.addEventListener('click', levelOne);
-// startBtn.addEventListener('click', levelTwo);
-// startBtn.addEventListener('click', levelThree);
-startBtn.addEventListener('click', endGame);
-
-
-//----------------------------FUNCTIONS----------------------//
-// Makes start btn clicks +1 to array
-function levelZero() {
-    if (levelArray.length !== 2) { 
-        levelArray.push(1); 
-    }
-    console.log(levelArray.length);
-};
-
-// ---------------------------LEVEL1-------------------------
-function levelOne(){
-    if (levelArray.length == 1) {
+//----------------STATES------------------------//
+var num;
+//----------------CACHE-------------------------//
+var displayEl = document.querySelector('h1 span');
+//----------------EVENT LISTENERS---------------//
+document.querySelector('button').addEventListener('click', function() {
+    num = num+1;
+    render();
+});
+//---------------RENDER/INIT---------------------//
+function init(){
+    num = 0;
+    render();
+}
+function render() {
+    displayEl.textContent = num;
+    lvlOne();
+    lvlTwo();
+    lvlThree();
+    endFunction()
+}
+init();
+//---------------LEVEL FUNCTIONS-------------------//
+function lvlOne(){
+    if (num === 1) {
+        // console.log('LVL1 FIRED');
         message.textContent = 'LEVEL 1: Where to start...?';
         start.innerHTML = '404 ERROR: Btn disabled...';
         document.getElementById('start').style.opacity = 0.4;
         document.getElementById('start').disabled = true;
-    };
-//SOLUTION => 
-//"document.getElementById('start').disabled = false;"
-};
-
-// //-----------------------------LEVEL2------------------------- 
-// function levelTwo(){
-//     if (levelArray.length == 2) {
-//         message.textContent = 'LEVEL 2: To Tart or Start?';
-//         start.innerHTML = 'TART GAME!';
-//         document.getElementById('start').id='tart';
-//     };
-// //SOLUTION
-// // document.getElementById('tart').id='start'
-// };
-
-// //----------------------------LEVEL3-----------------------------
-// function levelThree(){
-//     if (levelArray.length == 3) {
-//         message.textContent = 'LEVEL 3: GONE!';
-//         document.getElementById('start').remove();
-//     };
-// //SOLUTION =>
-// // document.body.innerHTML += '<button>START</button>';
-// // $0.id='start';
-// };
-
-//-----------------------------ENDGAME----------------------------
-//END GAME
-function endGame(){
-    if (levelArray.length >= 2) { 
-        document.getElementById('start');
-        start.innerHTML = 'START GAME!';
-        document.getElementById('start').style.opacity = 1;
-        document.getElementById('start').disabled = true;
-        message.textContent = 'You SEI rock! Thanks!';
     }
 };
-
-//----------------------FEHLER CHATBOT------------------------------
+function lvlTwo(){
+    if (num === 2) {
+        // console.log('LVL2 FIRED');
+        message.textContent = 'LEVEL 2: Make it stop...';
+        start.innerHTML = 'DOH!';
+        document.getElementById('start').style.opacity = 1;
+    }
+};
+function lvlThree(){
+    if (num === 3) {
+        // console.log('LVL3 FIRED');
+        message.textContent = 'LEVEL 3: *imagine message here*';
+        start.innerHTML = 'START GAME!';
+    }
+};
+//-----------------CHATBOT/TERMINAL----------------//
 //APPEND USER COMMUNICATIONS
 function textBox() {
     var li = document.createElement('li');
@@ -88,38 +58,11 @@ function textBox() {
     eval(input.value)
     // console.log(`this is the code as a string ${input.value}`);
 };
-
-
-//FEHLER COMMUNICATIONS PROMPTS
-// var fehlerSays() {
-//     for (var i = 0; i < ul.length; i++) {
-//         var ul = document.getElementById('myUl');
-//         var li = document.createElement('li');
-//         li.appendChild(document.createTextNode(name));
-//         ul.appendChild(li);
-//     }
-    // if (userArray === 'Y') {
-        
-    // }
-    // if () {
-
-    // }
-    // if () {
-
-    // }
-// };
-//--------------------------------------------------------------------
-console.log(levelArray.length);
-//------------------------------LEVELS END--------------------------//
-
-
-//BREAK
-// start.innerHTML = '>:D';
-// document.getElementById('start').style.backgroundColor='black';
-
-//COLOR CHECKER
-// if ($('#start').css('backgroundColor') == 'green') {
-//     console.log('GREEN BTN!');
-//   } else {
-//     console.log('NOT GREEN!');
-//   }
+//----------------END------------------------------//
+function endFunction(){
+    if (num === 4) {
+        message.textContent = 'YAY! Now we can start the game!';
+        start.innerHTML = 'START GAME!';
+        // console.log('GAME END');
+    }
+};
